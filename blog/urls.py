10eb6c0a1a1cebe25bdad916b39521f458f8aa9c
sagar <sagarbobby6@gmail.com>
2020-12-28 17:57:26 +0530

    added views to create edit/post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name = 'post_list'),
     path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
+    path('post/new/', views.create_post, name = 'create_post'),
+    path('post/<int:pk>/edit/', views.post_edit, name = 'post_edit')
 ]
